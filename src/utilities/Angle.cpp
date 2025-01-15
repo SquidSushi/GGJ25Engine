3b8fa6733bc0b0817ad3801bbc897f9e2aa2a86b
Oliver/SquidSushi_ <35111378+SquidSushi@users.noreply.github.com>
2025-01-15 23:46:16 +0100

    Added Angles and wrote Header

---
+++ src/utilities/Angle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mrbau on 15.01.2025.
+//
+
+#include "Angle.h"
---
+++ src/utilities/Angle.h
@@ -0,0 +1,37 @@
+//
+// Created by mrbau on 15.01.2025.
+//
+
+#ifndef RAYLIBSTARTER_ANGLE_H
+#define RAYLIBSTARTER_ANGLE_H
+
+
+class Angle { //Class for handling angles in Degrees
+    float value;
+    void normalize(); //Normalize the angle to be between 0 and 360; This has to be called in most operations.
+public:
+    //Constructor using a float
+    Angle(float value) : value(value) {}
+    // ##Operators##
+    //Addition
+    Angle operator+(Angle a);
+    Angle& operator+=(Angle a);
+    Angle operator+(float a);
+    Angle& operator+=(float a);
+    //Subtraction
+    Angle operator-(Angle a);
+    Angle& operator-=(Angle a);
+    Angle operator-(float a);
+    Angle& operator-=(float a);
+    // Conversion
+    operator float() const; //Convert the angle to a float
+    // Assignment
+    Angle& operator=(float a);
+
+    float asRadians(); //Convert the angle to Radians
+    static float AngleDifference(Angle a, Angle b); //Get the difference between two angles
+
+};
+
+
+#endif //RAYLIBSTARTER_ANGLE_H
